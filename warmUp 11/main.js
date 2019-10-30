25d276d52eedc2d81e24533a28698aee95f9710b
alijalal-sys <ajmabilal@gmail.com>
2019-10-30 10:05:51 +0100

    Warm Up 11 Done!

---
+++ warmUp 11/main.js
@@ -0,0 +1,9 @@
+// alert('Good Morning :)')
+
+$('#left-btn').on('click', function(){
+	$('#left-list').append('<li></li>')
+});
+
+$('#right-btn').on('click', function(){
+	$('#right-list').append('<li></li>')
+});

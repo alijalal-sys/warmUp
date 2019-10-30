da8988cfda2e8d02ecf2983e2b7b2129a4049d6f
alijalal-sys <ajmabilal@gmail.com>
2019-10-30 17:01:05 +0100

    Refactor Warm Up 11

--- warmUp 11/main.js
+++ warmUp 11/main.js
@@ -1,9 +1,12 @@
 // alert('Good Morning :)')
-
 $('#left-btn').on('click', function(){
-	$('#left-list').append('<li></li>')
+	$('#create-btn').on('click', function(){
+		$('#left-list').append('<li></li>')
+	})
 });
 
 $('#right-btn').on('click', function(){
-	$('#right-list').append('<li></li>')
+	$('#create-btn').on('click', function(){
+		$('#right-list').append('<li></li>')
+	})
 });

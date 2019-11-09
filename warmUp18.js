0161751643706e4c6386f558d5811fee9f5b786c
alijalal-sys <ajmabilal@gmail.com>
2019-11-09 09:45:02 +0100

    warm up 18 not finished yet i don't know why

--- warmUp18.js
+++ warmUp18.js
@@ -10,3 +10,17 @@
 
 // input   =  "aaaaaaaaaaaa"
 // result  =  "123456789101112"
+
+function replace(str){
+	var count = 1;
+	var j;
+	var arrOfChars = str.split('');
+	for (var i = 0; i < arrOfChars.length; i++) { // What i am trying to do is when iterate
+		j = i - 1; // it will make a variable called j that is less than i so when it is equal to 1 it will replace the index after it to i++
+		if(arrOfChars[j] === 1){ // I am missing something
+			arrOfChars.splice(i, 1, i++)
+		}
+		arrOfChars.splice(i, 1, count)
+	}
+	return arrOfChars;
+}
